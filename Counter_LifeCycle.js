c2501001df700707210c375fcf4dde412c1be4b2
Vedanth-Pranav <147240075+Vedanth-Pranav@users.noreply.github.com>
2025-05-07 10:01:25 +0530

    Update Counter_LifeCycle.js

--- Counter_LifeCycle.js
+++ Counter_LifeCycle.js
@@ -4,23 +4,16 @@ class Counter_LifeCycle extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
-      isIncrementing: true
+      count: 0
     };
     console.log('Constructor: Initialization');
   }
 
   componentDidMount() {
     console.log('ComponentDidMount: Component mounted');
-    this.startCounter();
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.isIncrementing !== this.state.isIncrementing) {
-      console.log('ComponentDidUpdate: Mode changed');
-      clearInterval(this.interval);
-      this.startCounter();
-    }
     if (prevState.count !== this.state.count) {
       console.log(`ComponentDidUpdate: Count updated to ${this.state.count}`);
     }
@@ -28,30 +21,26 @@ class Counter_LifeCycle extends React.Component {
 
   componentWillUnmount() {
     console.log('ComponentWillUnmount: Component is being unmounted');
-    clearInterval(this.interval);
   }
 
-  startCounter = () => {
-    this.interval = setInterval(() => {
-      this.setState(prevState => ({
-        count: prevState.isIncrementing ? prevState.count + 1 : prevState.count - 1
-      }));
-    }, 1000);
+  increment = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
   };
 
-  toggleMode = () => {
+  decrement = () => {
     this.setState(prevState => ({
-      isIncrementing: !prevState.isIncrementing
+      count: prevState.count - 1
     }));
   };
 
   render() {
     return (
       <div>
-        <h1>{this.state.isIncrementing ? 'Incrementing' : 'Decrementing'} Count: {this.state.count}</h1>
-        <button onClick={this.toggleMode}>
-          Switch to {this.state.isIncrementing ? 'Decrement' : 'Increment'}
-        </button>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
       </div>
     );
   }
